70a15030a0509614f9e8db244c7042fcb1fb78e7
Tyler Griggs <tegriggs7@gmail.com>
2023-10-05 02:43:35 +0000

    cpu affinity, queue interface, start thread pool

--- db_client.h
+++ db_client.h
@@ -1,105 +1,93 @@
 #ifndef DB_CLIENT_H
 #define DB_CLIENT_H
 
-#include <chrono>
 #include <iostream>
+#include <chrono>
 #include <thread>
 #include <vector>
 
-#include "atomicops.h"
 #include "readerwriterqueue.h"
+#include "atomicops.h"
 
 using namespace moodycamel;
 using namespace std;
 
-// TODO: if try_enqueue fails, need to try again
+// TODO: if try_enqueue fails, need to try again 
 // TODO: update all member variables to have _ suffix
 // TODO: add internaal metric state
 
 // TODO: create client arguments to modify request size, type, frequency, etc.
 struct SingleRead {
-  size_t start_idx;
-  size_t read_size;
+    size_t start_idx;
+    size_t read_size;
 
-  SingleRead(size_t start_idx, size_t read_size)
-      : start_idx(start_idx), read_size(read_size) {}
+    SingleRead(size_t start_idx, size_t read_size) : start_idx(start_idx), read_size(read_size) {}
 };
 
 struct DBRequest {
-  int client_id;
-  std::vector<SingleRead> reads;
+    int client_id;
+    std::vector<SingleRead> reads;
 
-  DBRequest(int client_id, std::vector<SingleRead> reads)
-      : client_id(client_id), reads(reads) {}
+    DBRequest(int client_id, std::vector<SingleRead> reads) : client_id(client_id), reads(reads) {}
 };
 
 class DBClient {
 public:
-  DBClient(const int client_id, ReaderWriterQueue<DBRequest> *request_queue,
-           const size_t db_size_elements, const size_t read_size,
-           const size_t num_reads)
-      : client_id(client_id), request_queue_(request_queue),
-        db_size_elements(db_size_elements), read_size(read_size),
-        num_reads(num_reads) {}
+    DBClient(const int client_id, ReaderWriterQueue<DBRequest>* request_queue, const size_t db_size_elements, const size_t read_size, const size_t num_reads) : 
+    client_id(client_id),
+    request_queue_(request_queue), 
+    db_size_elements(db_size_elements),
+    read_size(read_size),
+    num_reads(num_reads)  {}
 
-  std::thread RunSequential() {
-    srand(time(0));
-    cout << "Client ID " << client_id << " running Sequential." << endl;
-    std::thread client_thread(RunSequentialInternal, client_id, request_queue_,
-                              db_size_elements, read_size, num_reads);
-    return client_thread;
-  }
+    std::thread RunSequential() {
+        srand(time(0));
+        cout << "Client ID " << client_id << " running Sequential." << endl;
+        std::thread client_thread(RunSequentialInternal, client_id, request_queue_, db_size_elements, read_size, num_reads);
+        return client_thread;
+    }
 
-  std::thread RunRandom() {
-    srand(time(0));
-    cout << "Client ID " << client_id << " running Random." << endl;
-    std::thread client_thread(RunRandomInternal, client_id, request_queue_,
-                              db_size_elements, read_size, num_reads);
-    return client_thread;
-  }
+    std::thread RunRandom() {
+        srand(time(0));
+        cout << "Client ID " << client_id << " running Random." << endl;
+        std::thread client_thread(RunRandomInternal, client_id, request_queue_, db_size_elements, read_size, num_reads);
+        return client_thread;
+    }
 
 private:
-  const int client_id;
-  ReaderWriterQueue<DBRequest> *request_queue_;
-  const size_t db_size_elements;
-  const size_t read_size;
-  const size_t num_reads;
+    const int client_id;
+    ReaderWriterQueue<DBRequest>* request_queue_;
+    const size_t db_size_elements;
+    const size_t read_size;
+    const size_t num_reads;
 
-  static void RunSequentialInternal(const int client_id,
-                                    ReaderWriterQueue<DBRequest> *request_queue,
-                                    const size_t db_size_elements,
-                                    const size_t read_size,
-                                    const size_t num_reads) {
-    for (int i = 0; i < num_reads; ++i) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 1kqps
+    static void RunSequentialInternal(const int client_id, ReaderWriterQueue<DBRequest>* request_queue, const size_t db_size_elements, const size_t read_size, const size_t num_reads) {
+        for (int i = 0; i < num_reads; ++i) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));  // 1kqps
 
-      int start_idx = rand() % (db_size_elements - read_size);
-      std::vector<SingleRead> req(1, SingleRead(start_idx, read_size));
-      while (!request_queue->try_enqueue(DBRequest(client_id, req))) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-      }
+            int start_idx = rand() % (db_size_elements - read_size);
+            std::vector<SingleRead> req(1, SingleRead(start_idx, read_size));
+            while (!request_queue->try_enqueue(DBRequest(client_id, req))) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        }
+        cout << "Client ID " << client_id << " completed." << endl;
     }
-    cout << "Client ID " << client_id << " completed." << endl;
-  }
 
-  static void RunRandomInternal(const int client_id,
-                                ReaderWriterQueue<DBRequest> *request_queue,
-                                const size_t db_size_elements,
-                                const size_t read_size,
-                                const size_t num_reads) {
-    size_t num_random = 16;
-    for (int i = 0; i < num_reads; ++i) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1)); // 1kqps
-      std::vector<SingleRead> reqs;
-      for (int i = 0; i < num_random; ++i) {
-        int start_idx = rand() % (db_size_elements - read_size / num_random);
-        reqs.push_back(SingleRead(start_idx, read_size / num_random));
-      }
-      while (!request_queue->try_enqueue(DBRequest(client_id, reqs))) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-      }
+    static void RunRandomInternal(const int client_id, ReaderWriterQueue<DBRequest>* request_queue, const size_t db_size_elements, const size_t read_size, const size_t num_reads) {
+        size_t num_random = 1024;
+        for (int i = 0; i < num_reads; ++i) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));  // 1kqps
+            std::vector<SingleRead> reqs;
+            for (int i = 0; i < num_random; ++i) {
+                int start_idx = rand() % (db_size_elements - read_size / num_random);
+                reqs.push_back(SingleRead(start_idx, read_size / num_random));
+            }
+            while (!request_queue->try_enqueue(DBRequest(client_id, reqs))) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        }
+        cout << "Client ID " << client_id << " completed." << endl;
     }
-    cout << "Client ID " << client_id << " completed." << endl;
-  }
 };
 #endif
--- db_main.cc
+++ db_main.cc
@@ -1,8 +1,10 @@
-#include <chrono>
 #include <iostream>
-#include <numeric>
-#include <time.h>
 #include <vector>
+#include <chrono>
+#include <pthread.h>
+#include <sched.h>
+#include <time.h>
+#include <numeric>
 
 #include "db_client.h"
 #include "fair_db.h"
@@ -10,47 +12,73 @@
 using namespace std;
 using namespace moodycamel;
 
+void SetThreadAffinity(std::thread& t, int core_id) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);         // Initialize the CPU set
+    CPU_SET(core_id, &cpuset);  // Set the desired core
+
+    pthread_t native_thread_handle = t.native_handle();
+
+    if (pthread_setaffinity_np(native_thread_handle, sizeof(cpu_set_t), &cpuset) != 0) {
+        std::cerr << "Error setting thread CPU affinity." << std::endl;
+    }
+}
+
+void SetCurrentAffinity(int core_id) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);         // Initialize the CPU set
+    CPU_SET(core_id, &cpuset);  // Set the desired core
+
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) != 0) {
+        std::cerr << "Error setting current CPU affinity." << std::endl;
+    }
+}
+
 int main() {
-  srand(time(0));
-  size_t db_size = 2e9; // Number of integers in the db (cur ~= 8GB)
-  size_t datatype_size = sizeof(int);
+    srand(time(0));
+    size_t db_size = 4e9;   // Number of integers in the db (cur ~= 16GB)
+    size_t datatype_size = sizeof(int);
 
-  // TODO: These are client
-  size_t num_reads = 1;   // Number of requests per client
-  size_t read_size = 1e7; // Bytes per requst.  (cur ~= 10MB)
-  size_t max_outstanding = 10;
-  size_t num_runs = 1;
+    size_t num_reads = 1000;  // Number of requests per client
+    size_t read_size = 1e7;  // Bytes per requst.  (cur ~= 10MB)
+    size_t max_outstanding = 10;
+    size_t num_runs = 1;
 
-  std::vector<ReaderWriterQueue<DBRequest> *> queues;
+    for (int i = 0; i < num_runs; ++i) {
+        std::vector<ReaderWriterQueue<DBRequest>*> queues;
 
-  ReaderWriterQueue<DBRequest> q1(max_outstanding);
-  queues.push_back(&q1);
-  DBClient client1(1, &q1, db_size, read_size / datatype_size, num_reads);
-  std::thread client_thread1 = client1.RunSequential();
+        ReaderWriterQueue<DBRequest> q1(max_outstanding);
+        queues.push_back(&q1);
+        DBClient client1(1, &q1, db_size, read_size / datatype_size, num_reads);
+        std::thread client_thread1 = client1.RunSequential();
+        SetThreadAffinity(client_thread1, 0);
 
-  ReaderWriterQueue<DBRequest> q2(max_outstanding);
-  queues.push_back(&q2);
-  DBClient client2(2, &q2, db_size, read_size / datatype_size, num_reads);
-  std::thread client_thread2 = client2.RunRandom();
+        ReaderWriterQueue<DBRequest> q2(max_outstanding);
+        queues.push_back(&q2);
+        DBClient client2(2, &q2, db_size, read_size / datatype_size, num_reads);
+        std::thread client_thread2 = client2.RunRandom();
+        SetThreadAffinity(client_thread2, 1);
 
-  auto db = FairDB(db_size, queues);
-  db.Init();
+        SetCurrentAffinity(2);
+        auto db = FairDB(db_size, queues);
+        db.Init();
+        
 
-  // Run the DB
-  // auto results = db.Run();
-  RunStats stats = db.Run(num_reads * 2);
-  for (const auto durs : stats.query_durations) {
-    int duration_avg =
-        std::accumulate(durs.begin(), durs.end(), 0) / durs.size();
-    cout << "Avg: " << duration_avg << " (dummy=" << stats.dummy << ")" << endl;
-  }
-  cout << endl;
+        // Run the DB
+        // auto results = db.Run();
+        RunStats stats = db.Run(num_reads * 2);
+        for (const auto durs : stats.query_durations) {
+            int duration_avg = std::accumulate(durs.begin(), durs.end(), 0) / durs.size();
+            cout << "Avg: " << duration_avg << " (dummy=" << stats.dummy << ")" << endl;
+        }
+        cout << endl; 
 
-  // Then Run the clients
-  // for client in clients: client.Run()
+        // Then Run the clients
+        // for client in clients: client.Run()
 
-  client_thread1.join();
-  client_thread2.join();
+        client_thread1.join();
+        client_thread2.join();
+    }
 
-  return 0;
+    return 0;
 }
